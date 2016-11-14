7fbe64f9ab9a135703058a401019de817c3daf12
ravi4rails <ravionrails@gmail.com>
2016-11-14 16:30:53 +0530

    Added seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+s1 = Service.create(:name => "Electrician", :description => "service description pending")
+s2 = Service.create(:name => "Carpenter", :description => "service description pending")
+s3 = Service.create(:name => "Welder", :description => "service description pending")
+s4 = Service.create(:name => "Repairs", :description => "service description pending")
+s5 = Service.create(:name => "Party Organizers", :description => "service description pending")
+s6 = Service.create(:name => "Designers", :description => "service description pending")
+s7 = Service.create(:name => "Painters", :description => "service description pending")
+
+
+sc1 = ServiceSubCategory.create(name: "All Electronic Items", :description => "service description pending", :service_id => 4)
+sc2 = ServiceSubCategory.create(name: "Furnitures", :description => "service description pending", :service_id => 4)
+sc3 = ServiceSubCategory.create(name: "Motor", :description => "service description pending", :service_id => 4)
+
+sc4 = ServiceSubCategory.create(name: "Party Planner", :description => "service description pending", :service_id => 5)
+sc5 = ServiceSubCategory.create(name: "Decorators", :description => "service description pending", :service_id => 5)
+
+sc4 = ServiceSubCategory.create(name: "Web Designer", :description => "service description pending", :service_id => 6)
+sc5 = ServiceSubCategory.create(name: "Interior Designer", :description => "service description pending", :service_id => 6)
+
+
