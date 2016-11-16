443f1f53c7a5d8f5e8b5c908deddf47c7ba2c639
ravi4rails <ravionrails@gmail.com>
2016-11-16 18:34:04 +0530

    product bundle resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :products
   resources :categories
+  resources :product_bundles
   resources :services do
     member do
       get :service_sub_category
