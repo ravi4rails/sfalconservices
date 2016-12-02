03dea4356314b56d1c4ddcc8f08105b2e057989c
ravi4rails <ravionrails@gmail.com>
2016-12-02 19:44:16 +0530

    Include Product bundle in admin name space

--- app/controllers/product_bundles_controller.rb
+++ app/controllers/product_bundles_controller.rb
@@ -1,54 +1,9 @@
 class ProductBundlesController < ApplicationController
 
-  before_action :set_product_bundle, only: [:edit, :show, :update, :destroy]
-
-  def index
-    @product_bundles = ProductBundle.all
-  end
-
-  def new
-    @product_bundle = ProductBundle.new
-  end
-
-  def edit
-  end
+  before_action :set_product_bundle, only: [:show]
 
   def show
     render layout: 'general'
-  end
-
-  def create
-    @product_bundle = ProductBundle.new(bundle_params)
-
-    respond_to do |format|
-      if @product_bundle.save
-        format.html { redirect_to @product_bundle, notice: 'Product was successfully created.' }
-        format.json { render :show, status: :created, location: @product_bundle }
-      else
-        format.html { render :new }
-        format.json { render json: @product_bundle.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  def update
-    respond_to do |format|
-      if @product_bundle.update(bundle_params)
-        format.html { redirect_to @product_bundle, notice: 'Product Bundle was successfully updated.' }
-        format.json { render :show, status: :ok, location: @product_bundle }
-      else
-        format.html { render :edit }
-        format.json { render json: @product_bundle.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  def destroy
-    @product.destroy
-    respond_to do |format|
-      format.html { redirect_to product_bundles_url, notice: 'Product Bundle was successfully destroyed.' }
-      format.json { head :no_content }
-    end
   end
 
   private
@@ -57,7 +12,4 @@ class ProductBundlesController < ApplicationController
       @product_bundle = ProductBundle.friendly.find(params[:id])
     end
 
-    def bundle_params
-      params.require(:product_bundle).permit(:name, :description, :category_id)
-    end
 end
